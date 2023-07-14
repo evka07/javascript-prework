bb1da4e68759ca437309e72eee2b05b764c37941
evka07 <138365894+evka07@users.noreply.github.com>
2023-07-14 17:24:05 +0200

    production

--- js/script.js
+++ js/script.js
@@ -25,3 +25,40 @@ function getMoveName(argMoveId) {
         return 'unknown move';
     }
 }
+
+function displayResult(player, computer) {
+    const resultDiv = document.getElementById("message");
+    let resultText = '';
+
+    const playerMoveName = getMoveName(player);
+    const computerMoveName = getMoveName(computer);
+
+    if (player === computer) {
+        resultText = 'Remis! Obaj gracze wybrali ' + playerMoveName;
+    } else if (
+        (player === "paper" && computer === "rock") ||
+        (player === "rock" && computer === "scissors") ||
+        (player === "scissors" && computer === "paper")
+    ) {
+        resultText = 'Wygrałeś! Ty wybrałeś  ' + playerMoveName + ', a komputer ' + computerMoveName;
+    } else {
+        resultText = 'Przegrałeś! Ty wybierz ' + playerMoveName + ', a komputer ' + computerMoveName;
+    }
+
+    clearMessages();
+    printMessage(resultText);
+}
+
+function clickPlayer(e) {
+    const playerChoice = e.target.id;
+    const computerChoice = randomChoice();
+    displayResult(playerChoice, computerChoice);
+}
+
+const paperBtn = document.querySelector('#paper');
+const rockBtn = document.querySelector('#rock');
+const scissorsBtn = document.querySelector('#scissors');
+
+paperBtn.addEventListener('click',  clickPlayer);
+rockBtn.addEventListener('click',  clickPlayer);
+scissorsBtn.addEventListener('click',  clickPlayer);
